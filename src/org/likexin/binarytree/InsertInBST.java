eaf56afa848510abbaa978211135891a32d21350
shingo lee <578431912@qq.com>
2018-05-13 14:43:20 +0800

    update bst

--- src/org/likexin/binarytree/InsertInBST.java
+++ src/org/likexin/binarytree/InsertInBST.java
@@ -1,4 +1,19 @@
 package org.likexin.binarytree;
 
 public class InsertInBST {
+
+    public static TreeNode insert(TreeNode root, int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+            return root;
+        }
+        if (root.val > value) {
+            root.left = insert(root.left, value);
+        }
+        if (root.val < value) {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
 }
