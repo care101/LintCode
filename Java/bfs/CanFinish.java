f8d4c432aa72e03d5ae4b98b642628e6337359b2
Kexin-Li <hollalikexin@gmail.com>
2018-06-11 23:17:31 +0800

    update readme

---
+++ Java/bfs/CanFinish.java
@@ -0,0 +1,24 @@
+package org.likexin.bfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CanFinish {
+
+  public boolean canFinish(int numCourses, int[][] prerequisites) {
+    List[] edges = new ArrayList[numCourses];
+
+
+    return false;
+  }
+
+  public static void main(String[] args) {
+    int numCourses = 2;
+    List[] edges = new ArrayList[numCourses];
+
+    for (int i = 0; i < numCourses; i++) {
+      edges[i] = new ArrayList();
+    }
+  }
+
+}
