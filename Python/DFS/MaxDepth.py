7b8c4788616e82330d98c97231e0eb6d2a7f34c0
Kexin-Li <hollalikexin@gmail.com>
2018-06-11 23:03:11 +0800

    update readme

--- Python/DFS/MaxDepth.py
+++ Python/DFS/MaxDepth.py
@@ -11,19 +11,12 @@ class Solution:
     @param root: The root of binary tree.
     @return: An integer
     """
-    def findDepth(self, root):
-      depth_l = 1
-      depth_r = 1
-      if root.left:
-        depth_l += self.findDepth(root.left)
-      if root.right:
-        depth_r += self.findDepth(root.right)
-      return max(depth_l, depth_r)
-      
     def maxDepth(self, root):
         # write your code here
+        depth = 1
         if not root:
           return 0
-        return self.findDepth(root)
+        depth += max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return depth
         
 
