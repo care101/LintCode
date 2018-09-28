bc2ab134e99c1281f1267683361e6f7890b8a1cf
SuyuanLiu <suyuanliu0618@gmail.com>
2018-09-28 17:01:36 +0800

    update preorder traversal;

--- Python3/binarytree/preorderTraversal.py
+++ Python3/binarytree/preorderTraversal.py
@@ -1,3 +1,4 @@
+# -*- coding:UTF-8 -*-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -10,17 +11,34 @@ class Solution:
         """
         :type root: TreeNode
         :rtype: List[int]
+        @ Solution: 递归 & 非递归 
+        - 递归: 按照先根节点，左节点，右节点递归即可；
+        - 非递归：利用栈，对于当前的root，先压入右节点，再压入左节点；当栈不为空，就一直遍历；
         """
+
+        # Solution2: recursion        
         if not root:
-            return []
-        
+            return []        
         ans = []
         ans.append(root.val)
-        temp_left, temp_right = [], []
-        if root.left:
-            temp_left = self.preorderTraversal(root.left)
-        if root.right:
-            temp_right = self.preorderTraversal(root.right)
-        ans = ans + temp_left + temp_right  
+        ans = ans + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
         return ans        
+
+
+
+        # Solution 1: loop
+        if not root:
+            return [] 
+        ans = []
+        stack = []
+        stack.append(root)
+        while(stack):
+            temp = stack.pop()
+            ans.append(temp.val)
+            if temp.right:
+                stack.append(temp.right)
+            if temp.left:
+                stack.append(temp.left)
+        return ans
+
 
